b140613bb44a5a7a64c05e657430be296318a269
szymon <szymon.kowalewski@gmail.com>
2021-06-10 17:36:43 +0200

    First commit

---
+++ src/main.cpp
@@ -0,0 +1,19 @@
+/* Add button press interrupt */
+/* Add image capture */
+/* Add image saver */
+/* Add signaling */
+
+void on_trigger()
+{
+    /* Set capture flag to true */
+}
+
+int main()
+{
+    /* Turn on the application diode */
+
+    /* Main loop */
+    while(true)
+    {
+    }
+}
